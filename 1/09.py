5c9148b5998411e134977f2b4cea3885ecaddfb3
yyamada12 <12yacropolisy@gmail.com>
2020-04-13 21:46:02 +0900

    make chapter directory

--- 1/09.py
+++ 1/09.py
@@ -5,13 +5,13 @@ import random
 def shuffle_long_words_of_sentence(str):
     def shuffle_long_word(word):
         if len(word) <= 4:
-            return word
+            yield word
         else:
             first_chr = word[0]
             middle_chrs = word[1:-1]
             end_chr = word[-1]
             shuffled_middle_chrs = ''.join(random.sample(middle_chrs, len(middle_chrs)))
-            return first_chr + shuffled_middle_chrs + end_chr
+            yield first_chr + shuffled_middle_chrs + end_chr
 
     return ' '.join(shuffle_long_word(word) for word in str.split())
 
