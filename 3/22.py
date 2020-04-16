963bb1e0d4dcb3cdeb206cc734246530cce117a3
yyamada12 <12yacropolisy@gmail.com>
2020-04-16 19:27:48 +0900

    22

---
+++ 3/22.py
@@ -0,0 +1,13 @@
+import json
+import gzip
+import re
+
+with gzip.open('jawiki-country.json.gz') as rf:
+    for line in rf:
+        obj = json.loads(line)
+        if obj['title'] == 'イギリス':
+            text = obj['text']
+            break
+categories = re.findall(r'\[\[Category:(.*?)(?:\|.*)?\]\]', text)
+for category in categories:
+    print(category)
