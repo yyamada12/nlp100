b3abc6489c18263e8b86a34ab5794ca855675f70
yyamada12 <12yacropolisy@gmail.com>
2020-04-17 09:19:28 +0900

    27

---
+++ 3/27.py
@@ -0,0 +1,22 @@
+import json
+import gzip
+import re
+
+with gzip.open('jawiki-country.json.gz') as rf:
+    for line in rf:
+        obj = json.loads(line)
+        if obj['title'] == 'イギリス':
+            text = obj['text']
+            break
+basic_info_string = re.search(r'{{基礎情報.*?(.*)^}}$', text,
+                              flags=(re.MULTILINE | re.DOTALL))
+basic_infos = re.findall(r'^\|(.*?) *\= *(.*?) *(?:(?=\n\|)|(?=\n$))',
+                         basic_info_string.group(1), flags=(re.MULTILINE | re.DOTALL))
+
+basic_info_dict = {}
+for key, value in basic_infos:
+    no_emphasis_value = re.sub(r"'{2,}", "", value)
+    no_inner_link_value = re.sub(
+        r"\[\[(?:[^\|]*\|)??(([^\|]*?)|({{.*?}}))\]\]", r"\1", no_emphasis_value)
+    basic_info_dict[key] = no_inner_link_value
+    print("'" + key + "', " + "'" + no_inner_link_value + "'")
