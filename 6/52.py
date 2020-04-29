be8ff2c2ec3a7db469c9fe6a0a3cb88b97f73b6a
yyamada12 <12yacropolisy@gmail.com>
2020-04-29 18:05:52 +0900

    52

--- 6/52.py
+++ 6/52.py
@@ -3,9 +3,9 @@ from sklearn.linear_model import LogisticRegression
 import pandas as pd
 
 
-train_X = pd.read_table('train.feature.txt', header=None)
-train_y = pd.read_table('train.label.txt', header=None)
+train_X = pd.read_table('train.feature.txt')
+train_y = pd.read_table('train.label.txt')
 
 lr = LogisticRegression(random_state=0)
-lr.fit(train_X, train_y[0])
+lr.fit(train_X, train_y['CATEGORY'])
 joblib.dump(lr, '52.Joblib')
