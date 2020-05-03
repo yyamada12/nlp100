0b3a436050a5d0e395294a6218613568fe2b11c6
yyamada12 <12yacropolisy@gmail.com>
2020-05-03 10:19:17 +0900

    63

---
+++ 7/63.py
@@ -0,0 +1,6 @@
+from gensim.models import KeyedVectors
+
+model = KeyedVectors.load_word2vec_format(
+    './GoogleNews-vectors-negative300.bin', binary=True)
+
+print(model.most_similar(positive=['Spain', 'Athens'],  negative=['Madrid']))
